c4d102ebae1aefa17a5b3f11041254d687f256b1
Sahid Hossain Mustakim <sratul35@gmail.com>
2023-08-04 21:50:08 +0600

    Added Create&Join Page in Circle

--- App.js
+++ App.js
@@ -1,60 +1,90 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { AuthContext, AuthProvider } from './src/providers/AuthProviders';
-import { NavigationContainer } from '@react-navigation/native';
-import LogIn from './src/screens/auth/LogIn';
-import Dummy from './src/screens/dashboard/Dummy';
-import SignUp from './src/screens/auth/SignUp';
-import BookingList from './src/screens/booking/BookingList';
-import NurseHighlight from './src/screens/booking/NurseHighlight';
-import BookingScreen from './src/screens/booking/BookingScreen';
+import { StyleSheet, SafeAreaView } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { AuthContext, AuthProvider } from "./src/providers/AuthProviders";
+import { NavigationContainer } from "@react-navigation/native";
+import LogIn from "./src/screens/auth/LogIn";
+import Dummy from "./src/screens/dashboard/Dummy";
+import SignUp from "./src/screens/auth/SignUp";
+import BookingList from "./src/screens/booking/BookingList";
+import NurseHighlight from "./src/screens/booking/NurseHighlight";
+import BookingScreen from "./src/screens/booking/BookingScreen";
+import JoinFamilyCircleModal from "./src/screens/dashboard/JoinFamilyCircleModal";
+import CreateFamilyCircleModal from "./src/screens/dashboard/CreateFamilyCircleModal";
 
-
-const AuthStack = createStackNavigator()
-const HomeStack = createStackNavigator()
-
+const AuthStack = createStackNavigator();
+const HomeStack = createStackNavigator();
 
 const AuthStackScreens = () => {
   return (
-    <AuthStack.Navigator initialRouteName='login'>
-      <AuthStack.Screen name='login' component={LogIn} options={{ headerShown: false }} />
-      <AuthStack.Screen name='signup' component={SignUp} options={{ headerShown: false }} />
-      {/* <AuthStack.Screen name='signup' options={{ headerShown: false }} /> */}
+    <AuthStack.Navigator initialRouteName="login">
+      <AuthStack.Screen
+        name="login"
+        component={LogIn}
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="signup"
+        component={SignUp}
+        options={{ headerShown: false }}
+      />
     </AuthStack.Navigator>
-  )
-}
-
+  );
+};
 
 const HomeStackScreens = ({ user }) => {
   return (
     <HomeStack.Navigator>
       {/* SADIA OPEN THIS COMMENT AFTER YOURE DONE WITH ACCOUNT SELECTION */}
       {/* <HomeStack.Screen name='account-selection' component={AccountSelection} /> */}
-      <HomeStack.Screen name='dummy' component={Dummy} options={{ headerShown: true, headerTitle: `Welcome!` }} />
-      <HomeStack.Screen name='booking-list' component={BookingList} options={{ headerShown: true, headerTitle: "All nurses" }} />
-      <HomeStack.Screen name='nurse-highlight' component={NurseHighlight} options={{ headerTitle: "View nurse", headerShown: true }} />
-      <HomeStack.Screen name='nurse-booking' component={BookingScreen} options={{ headerTitle: "Book nurse", headerShown: true }} />
+      <HomeStack.Screen
+        name="dummy"
+        component={Dummy}
+        options={{ headerShown: true, headerTitle: `Welcome!` }}
+      />
+      <HomeStack.Screen
+        name="booking-list"
+        component={BookingList}
+        options={{ headerShown: true, headerTitle: "All nurses" }}
+      />
+      <HomeStack.Screen
+        name="nurse-highlight"
+        component={NurseHighlight}
+        options={{ headerTitle: "View nurse", headerShown: true }}
+      />
+      <HomeStack.Screen
+        name="nurse-booking"
+        component={BookingScreen}
+        options={{ headerTitle: "Book nurse", headerShown: true }}
+      />
+      <HomeStack.Screen
+        name="join-family-circle"
+        component={JoinFamilyCircleModal}
+        options={{ headerTitle: "Join family circle", headerShown: true }}
+      />
+      <HomeStack.Screen
+        name="create-family-circle"
+        component={CreateFamilyCircleModal}
+        options={{ headerTitle: "Create family circle", headerShown: true }}
+      />
     </HomeStack.Navigator>
-  )
-}
-
+  );
+};
 
 export default function App() {
-
   return (
     <AuthProvider>
       <AuthContext.Consumer>
-        {(
-          (authCtx) => (
-            <NavigationContainer>
-              {authCtx.isLoggedIn ? <HomeStackScreens user={authCtx.userCache} /> : <AuthStackScreens />}
-            </NavigationContainer>
-          )
+        {(authCtx) => (
+          <NavigationContainer>
+            {authCtx.isLoggedIn ? (
+              <HomeStackScreens user={authCtx.userCache} />
+            ) : (
+              <AuthStackScreens />
+            )}
+          </NavigationContainer>
         )}
       </AuthContext.Consumer>
     </AuthProvider>
     // <LogIn />
   );
 }
-
-
--- src/screens/dashboard/Dummy.js
+++ src/screens/dashboard/Dummy.js
@@ -1,14 +1,20 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Button } from "@rneui/base";
-import { invokeLogoutService } from "../../services/user/authService";
 import { AuthContext } from "../../providers/AuthProviders";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/firebaseConfigs";
 import { SpeedDial } from "@rneui/themed";
+import JoinFamilyCircleModal from "./JoinFamilyCircleModal";
+import CreateFamilyCircleModal from "./CreateFamilyCircleModal";
 
 const Dummy = (props) => {
-  const onLogoutButtonPress = (authCtx) => {
+  const authCtx = useContext(AuthContext);
+
+  const [joinModalVisible, setJoinModalVisible] = useState(false);
+  const [createModalVisible, setCreateModalVisible] = useState(false);
+
+  const onLogoutButtonPress = () => {
     invokeLogoutService(authCtx.userCache);
     signOut(auth);
     authCtx.setUserCache([]);
@@ -17,46 +23,58 @@ const Dummy = (props) => {
 
   const [open, setOpen] = React.useState(false);
 
-  return (
-    <AuthContext.Consumer>
-      {(authCtx) => (
-        <View style={{ padding: 30 }}>
-          <Text>This is a test dashboard</Text>
-          <View style={{ margin: 10 }}>
-            <Button
-              title="BOOKING"
-              onPress={() => props.navigation.navigate("booking-list")}
-            />
-          </View>
+  const handleJoin = (joinLink) => {
+    console.log("Joining with link:", joinLink);
+  };
 
-          <View style={{ margin: 10 }}>
-            <Button
-              color="red"
-              title="LOGOUT"
-              onPress={() => onLogoutButtonPress(authCtx)}
-            />
-          </View>
-          <SpeedDial
-            isOpen={open}
-            icon={{ name: "edit", color: "#fff" }}
-            openIcon={{ name: "close", color: "#fff" }}
-            onOpen={() => setOpen(!open)}
-            onClose={() => setOpen(!open)}
-          >
-            <SpeedDial.Action
-              icon={{ name: "add", color: "#fff" }}
-              title="Join"
-              onPress={() => console.log("Add Something")}
-            />
-            <SpeedDial.Action
-              icon={{ name: "create", color: "#fff" }}
-              title="Add"
-              onPress={() => console.log("Delete Something")}
-            />
-          </SpeedDial>
-        </View>
-      )}
-    </AuthContext.Consumer>
+  const handleCreate = (familyCircleName) => {
+    console.log("Creating family circle:", familyCircleName);
+  };
+
+  return (
+    <View style={{ padding: 30 }}>
+      <Text>This is a test dashboard</Text>
+      <View style={{ margin: 10 }}>
+        <Button
+          title="BOOKING"
+          onPress={() => props.navigation.navigate("booking-list")}
+        />
+      </View>
+
+      <View style={{ margin: 10 }}>
+        <Button color="red" title="LOGOUT" onPress={onLogoutButtonPress} />
+      </View>
+      <View style={{ padding: 200, bottom: -150, left: -50 }}>
+        <SpeedDial
+          isOpen={open}
+          icon={{ name: "edit", color: "#fff" }}
+          openIcon={{ name: "close", color: "#fff" }}
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+        >
+          <SpeedDial.Action
+            icon={{ name: "add", color: "#fff" }}
+            title="Create Family Circle"
+            onPress={() => setCreateModalVisible(true)}
+          />
+          <SpeedDial.Action
+            icon={{ name: "create", color: "#fff" }}
+            title="Join Family Circle"
+            onPress={() => setJoinModalVisible(true)}
+          />
+        </SpeedDial>
+        <JoinFamilyCircleModal
+          visible={joinModalVisible}
+          onClose={() => setJoinModalVisible(false)}
+          onJoin={handleJoin}
+        />
+        <CreateFamilyCircleModal
+          visible={createModalVisible}
+          onClose={() => setCreateModalVisible(false)}
+          onCreate={handleCreate}
+        />
+      </View>
+    </View>
   );
 };
 
