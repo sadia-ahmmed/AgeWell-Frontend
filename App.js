c6772d1e1a8baf7085259e808af23d5386c917c4
Tamanna Shermin <tshermin201130@bscse.uiu.ac.bd>
2023-08-15 15:19:00 +0600

    space added in app.js

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ export default function App() {
     
       <NavigationContainer>
         <AppNavigator />
+                               
+                               
       </NavigationContainer>
    
   );
